131c1ed4f20c4737348b026af6673507e823cbad
Marek <marek.piekarz@gmail.com>
2019-11-30 22:13:27 +0100

    lab6

---
+++ Lab6/Lab6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Zadanie1 z1 = new Zadanie1();
+        }
+    }
+}
---
+++ Lab6/Lab6/Zadanie1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Lab6
+{
+    public class Zadanie1
+    {
+        public Zadanie1()
+        {
+            var b1 = new Fir.Bauble();
+            var b2 = new Fir.Bauble();
+            var b3 = new Fir.Bauble();
+            b1.Add("red", "ball");
+            b2.Add("blue", "star");
+            b3.Add("green", "angel");
+        }
+    }
+
+    public class Tree
+    {
+        string name;
+        int age;
+        public Tree() { }
+        public Tree(string _name, int _age)
+        {
+            name = _name;
+            age = _age;
+        }
+    }
+
+    public class Fir : Tree
+    {
+        protected class Bauble
+        {
+            string color;
+            string type;
+
+            internal void Add(string _color, string _type)
+            {
+                color = _color;
+                type = _type;
+            }
+
+
+        }
+    }
+
+    public class ChristmassTree : Fir
+    {
+        public ChristmassTree(string _name, int _age)
+        {
+            name = _name;
+            age = _age; 
+        }
+    }
+}
